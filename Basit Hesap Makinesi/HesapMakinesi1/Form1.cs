05732b0663819aa27c414755ae4ea06b8caae6fb
Mert Yıldız <mert2618yildiz@gmail.com>
2023-08-24 16:37:37 +0300

    simple calculator
    coded in C#

---
+++ Basit Hesap Makinesi/HesapMakinesi1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        // Field, Alan Form1 in değişkenleri, nesneleri
+        // HesapMakinasu sınıfından yeni bir nesne örneği hm : 
+        HesapMakinesi hm = new HesapMakinesi();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // sayı 1   sayı 2 hesapla
+            //double sayi1 = Convert.ToDouble(textBox1.Text);
+            //double sayi2 = double.Parse(textBox2.Text);
+            double sayi1, sayi2;
+            hm.Sayilarial(textBox1.Text, textBox2.Text, out sayi1, out sayi2);
+            double toplam = hm.Topla(sayi1, sayi2);
+            label3.Text = hm.Gosterim(sayi1, sayi2, toplam, " + ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // sayı 1 - sayı 2 hesapla
+            //double sayi1 = Convert.ToDouble(textBox1.Text);
+            //double sayi2 = double.Parse(textBox2.Text);
+            double sayi1, sayi2;
+            hm.Sayilarial(textBox1.Text, textBox2.Text, out sayi1, out sayi2);
+            double fark = hm.Cikar(sayi1, sayi2);
+            label3.Text = hm.Gosterim(sayi1, sayi2, fark, " - ");
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // sayı 1 * sayı 2 hesapla
+            //double sayi1 = Convert.ToDouble(textBox1.Text);
+            //double sayi2 = double.Parse(textBox2.Text);
+            double sayi1, sayi2;
+            hm.Sayilarial(textBox1.Text, textBox2.Text, out sayi1, out sayi2);
+            double çarpim = hm.Carp(sayi1, sayi2);
+            label3.Text = hm.Gosterim(sayi1, sayi2, çarpim, " x ");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // sayı 1 / sayı 2 hesapla
+            //double sayi1 = Convert.ToDouble(textBox1.Text);
+            //double sayi2 = double.Parse(textBox2.Text);
+            double sayi1, sayi2;
+            hm.Sayilarial(textBox1.Text, textBox2.Text, out sayi1, out sayi2);
+            double bölüm = hm.Böl(sayi1, sayi2);
+            label3.Text = hm.Gosterim(sayi1, sayi2, bölüm, " / ");
+        }
+    }
+}
---
+++ Basit Hesap Makinesi/HesapMakinesi1/HesapMakinasi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    internal class HesapMakinesi
+    {
+        // Field : Alan : Değişkenler
+        // Property : Özellik: Private alanlara kontrollü okuma ,yazma sağlar
+        // Metotlar : Nesnenin fonksiyonları 
+        public void Sayilarial(string s1, string s2, out double sayi1, out double sayi2)
+        {
+            // Hata olurda sayi1 ve sayi 2 değer almaz ise, başlangıç değeri verelim
+            sayi1 = sayi2 = 0;
+            try
+            {
+                sayi1 = Convert.ToDouble(s1);
+                sayi2 = double.Parse(s2);
+            }
+            catch (Exception istisna)
+            {
+                DialogResult result = MessageBox.Show("Girdi Yanlış Sayı Girmelisin!!", "Hata Penceresi",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            }
+        }
+        public string Gosterim(double sayi1, double sayi2, double sonuç, string islem)
+        {
+            string gösterilecekIfade = sayi1.ToString() +islem + sayi2.ToString() +  " = " +
+                                       sonuç.ToString();
+            return gösterilecekIfade;
+        }
+        public double Topla(double s1 ,double s2)
+        {
+            double sonuc = s1 + s2;
+            return sonuc;
+        }
+        public double Cikar(double s1, double s2)
+        {
+            double sonuc = s1 - s2;
+            return sonuc;
+        }
+        public double Carp(double s1, double s2)
+        {
+            double sonuc = s1 * s2;
+            return sonuc;
+        }
+        public double Böl(double s1, double s2)
+        {
+            double sonuc = s1 / s2;
+            return sonuc;
+        }
+    }
+}
